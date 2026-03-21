bfc48c5e399ba19a70ab33a232936befa7bb8045
Faheem101p <88618486+Faheem101p@users.noreply.github.com>
2026-03-21 15:24:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+const sequence = [
+"reproduce",
+"inspect",
+"logs",
+"breakpoint",
+"config",
+"fix",
+"verify"
+];
+
+let currentStep = 0;
+
+function log(msg){
+document.getElementById("log").innerHTML += "> " + msg + "<br>";
+}
+
+function resetSystem(reason){
+currentStep = 0;
+
+let logBox = document.getElementById("log");
+logBox.innerHTML = "";
+
+log("RESET: " + reason);
+log("Hint: Start from reproducing the issue");
+}
+
+/* main step handler */
+
+function step(action){
+
+document.getElementById("success").classList.add("hidden");
+
+if(action !== sequence[currentStep]){
+resetSystem("Incorrect debugging step");
+return;
+}
+
+currentStep++;
+
+
+switch(action){
+
+case "reproduce":
+log("Issue reproduced successfully");
+break;
+
+case "inspect":
+log("Network requests analyzed");
+break;
+
+case "logs":
+log("Error logs identified");
+break;
+
+case "breakpoint":
+log("Breakpoint set, execution paused");
+break;
+
+case "config":
+log("Configuration validated");
+break;
+
+case "fix":
+log("Fix applied to code");
+break;
+
+case "verify":
+log("Verifying fix...");
+log("System stable");
+break;
+
+}
+
+/* success */
+
+if(currentStep === sequence.length){
+document.getElementById("success").classList.remove("hidden");
+}
+
+}
